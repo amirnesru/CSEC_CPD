fb8146d9bcfe69494a59735ef43a2aa1baffa4d7
amiines <amirnesru054@gmail.com>
2026-02-18 09:20:59 +0300

    Create 	Dagatal.py

---
+++ Dagatal.py
@@ -0,0 +1,13 @@
+m=int(input())
+if m<=7:
+    if m%2!=0:
+        print(31)
+    elif m==2:
+        print(28)
+    else:
+        print(30)        
+else:
+    if m%2==0:
+        print(31)
+    else:
+        print(30)            
